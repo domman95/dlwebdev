e5e304ded78e8a3a4097755e4cf76cd0bd7a5842
domman95 <dominiklasek95@gmail.com>
2021-08-04 22:27:12 +0200

    changed gatsby-config and netlify.toml files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "dlwebdev",
+    siteUrl: 'https://dlwebdev.com',
+    title: 'dlwebdev',
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: ['gatsby-plugin-styled-components'],
 };
